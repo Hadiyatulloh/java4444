e52bf50ce86e7ae6cec1ec94f9b0e0c1770efddb
Hadiyatulloh <hadiyatullohfayzullayev@gmail.com>
2024-12-24 15:25:10 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,50 @@
+//  let person = {
+//      name: "xadado",
+//      age: 30,
+//      isStudent: false
+//  }
+//  let car = new object();
+//  car.brand = 'tesla';
+//  car.medel = 'model s';
+//  car.year = 2022;
+
+//  console.log(person.name);
+//  console.log(person.age);
+
+
+//  console.log(person["name"]);
+//  console.log(person["age"]);
+
+
+//  person.city = "New York";
+//  person.age(person["age"]);
+
+
+//  delete person.isStudent;
+
+
+// let film = {
+//     title: '2112',
+//     director: "hadiyatulloh",
+//     realeseYear: 2024,
+//     genres: "anime",
+//     win: true
+// }
+// alert(film.director)
+
+// if (film.genres = komediya) {
+//     alert(`${genres} komediya`)
+// } else(
+//     alert(`${genres} bu ${genres}`)
+// )
+
+let nam = prompt('ismizzi kiritin')
+let id = prompt('space idyizzi kiritin')
+let password = prompt('space parolizzi kiritin')
+
+let a = {
+    name: `${nam}`,
+    id: `${id}`,
+    password: `${password}`
+}
+console.log(a)
